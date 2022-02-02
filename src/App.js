69664e0e9737f738c520280d56f960bcd3bf25a2
Philippe Ngom <philngom@gmail.com>
2022-02-02 14:24:27 -0800

    set up GoblinList to pass goblin to Goblin

--- src/App.js
+++ src/App.js
@@ -76,7 +76,7 @@ function App() {
         setGoblinFormHP={setGoblinFormHP}
       />
       <GoblinList
-        goblins={[]} // this takes in an array of goblins. If the filteredGoblins has a length, use that array. Otherwise, use the allGoblins array
+        goblins={ allGoblins } // this takes in an array of goblins. If the filteredGoblins has a length, use that array. Otherwise, use the allGoblins array
         handleDeleteGoblin={handleDeleteGoblin} // note that the goblin list has access to the ability to delete
       />
     </div>
